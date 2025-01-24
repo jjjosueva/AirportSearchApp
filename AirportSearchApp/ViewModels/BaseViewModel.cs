214f30ef1d71db6308e888bd24bb3536415ec57c
jjjosueva <alexanderjosue192004@gmail.com>
2025-01-24 07:46:53 -0500

    Agregue BaseViewModel Para su respetiva  programacion para la conexion con AirportViewModel

---
+++ AirportSearchApp/ViewModels/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+public class BaseViewModel : INotifyPropertyChanged
+{
+  
+    public event PropertyChangedEventHandler PropertyChanged;
+
+   
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+   
+    protected bool SetProperty<T>(ref T field, T value, string propertyName)
+    {
+        if (!EqualityComparer<T>.Default.Equals(field, value))
+        {
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        return false;
+    }
+}
